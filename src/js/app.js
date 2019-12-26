cdea2a243c0cda7b72b3c374a31847d6d741911b
ekkebus <s.p.ekkebus@gmail.com>
2019-12-26 20:01:49 +0100

    1) Refactored js functions to arrow functions. 2) Added ga event logging in app.js 3) Changed default text in html

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,9 @@
 /** 
  * -------------
- * SPA app
+ * Retrogenerator SPA app
  * -------------
 */
-spa = (function () {
+spa = ( () => {
     var configMap = {
         templateId: 'spa',
         questionDomId: 'question'
@@ -11,9 +11,9 @@ spa = (function () {
     stateMap = {
         $container: undefined
     },
-    _initModule, _showNextQuestion;
+    _initModule, _showNextQuestion, _roundCounter = 0;
 
-    _initModule = function ($container) {
+    _initModule = ($container) => {
         stateMap.$container = $container;
 
         let dataLoaded = spa.data.initModule();
@@ -21,68 +21,103 @@ spa = (function () {
 
         Promise.all([dataLoaded,modelLoaded]).then(data => {
             _showNextQuestion();
+            _roundCounter++;
+            spa.model.logEvent('OnClick',_roundCounter);
             
-            document.getElementById(stateMap.$container).onclick = function() { 
+            document.getElementById(stateMap.$container).onclick = () => { 
                 _showNextQuestion(); 
+                _roundCounter++;
+                spa.model.logEvent('OnClick',_roundCounter);
             };
             
-            //.addEventListener("click", _showNextQuestion(), false);
         }).catch(error => {
-           console.error('spa.initModule ' + error);
+           console.error(`spa.initModule ${error}`);
         });
-    };
+    }
 
-    _showNextQuestion = function(){
+    _showNextQuestion = () => {
         document.getElementById(configMap.questionDomId).innerHTML = spa.model.getQuestion();
-    };
+    }
 
     //public API
     return {
         initModule: _initModule
     };
-}());
+})();
 
 /** 
  * -------------
  * MODEL 
  * -------------
 */
-spa.model = (function () {
+spa.model = ( () => {
     var configMap = {
     },
     stateMap = {
-        $questions: undefined
+        $questions: undefined,
+        $currentQuestion: undefined,
+        $currentQuestionStartTime: undefined
     },
     _initModule, _getQuestion, _shuffleArray;
 
     //returns a promise when it is ready
-    _initModule = function () {
+    _initModule = () => {
         console.log('spa.model.initModule');
         localStorage.clear();
+        stateMap.$currentQuestionStartTime = new Date().getTime();
+
         return spa.data.loadData().then(json => {
             //shuffle the array and store it
             stateMap.$questions = _shuffleArray(json.questions);
         });
-    };
+    }
 
-    _getQuestion = function () {
+    _getQuestion = () => {
         console.log('spa.model.getQuestion');
         if(stateMap.$questions == undefined || stateMap.$questions.length == 0){
             return "Well done! You are done! :)";
         }
-        return stateMap.$questions.pop().question;
-    };
+        //if there is a previous currentQuestion defined
+        if(typeof stateMap.$currentQuestion !== 'undefined'){
+            _logEvent('Time',`Viewed question ${stateMap.$currentQuestion.id}`,Math.round((new Date().getTime() - stateMap.$currentQuestionStartTime) / 1000));
+            stateMap.$currentQuestionStartTime = new Date().getTime();
+        }
 
-    _shuffleArray = function(array){
+        stateMap.$currentQuestion = stateMap.$questions.pop();
+        
+        return stateMap.$currentQuestion.question;
+    }
+
+    //logs action events (from UI)
+    _logEventAction = (eventName, eventValue) => {
+        _logEvent('Actions',eventName,eventValue);
+    }
+
+    _logEvent = (eventCategory,eventName, eventValue) => {
+        console.log(`Send ${eventCategory} Event to GA: ${eventName}  with value ${eventValue}`);
+        //add event to analytics
+        if(typeof ga === "function"){
+            ga('send', {
+                hitType: 'event',
+                eventCategory: eventCategory,
+                eventAction: eventName,
+                eventValue: eventValue
+            });
+        }
+        else console.log("ga('send', 'event',...) not defined.");
+    }
+
+    _shuffleArray = (array) => {
         return array.sort(() => Math.random() - 0.5);
     }
 
     //public API
     return {
         initModule: _initModule,
-        getQuestion: _getQuestion
+        getQuestion: _getQuestion,
+        logEvent: _logEventAction
     };
-}());
+})();
 
 /** 
  * -------------
@@ -90,7 +125,7 @@ spa.model = (function () {
  * -------------
 */
 
-spa.data = (function () {
+spa.data = ( () => {
     var configMap = {
         endpoint: './src/questions-nl.json'
     },
@@ -100,15 +135,15 @@ spa.data = (function () {
     initModule;
 
     //returns a promise when it is ready
-    _initModule = function () {
-        return new Promise(function(resolve, reject) {
+    _initModule =  () => {
+        return new Promise((resolve, reject) => {
             console.log('spa.data.initModule');
             setTimeout(resolve, 100);
           });
-    };
+    }
 
     // load data returns a promise which contains json data
-    _loadData = function () {
+    _loadData = () => {
         console.log('spa.data.loadData');
 
         return fetch(configMap.endpoint).then(response => response.json())
@@ -117,11 +152,11 @@ spa.data = (function () {
             return json;
         });
 
-    };
+    }
 
     //public API
     return {
         initModule: _initModule,
         loadData: _loadData
     };
-}());
+})();
